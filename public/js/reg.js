770498567d2a177da019a82392fa1b02a3c7b947
glindaqu <q5481996@gmail.com>
2024-04-15 03:47:58 +0700

    project struct update

---
+++ public/js/reg.js
@@ -0,0 +1,7 @@
+const email = document.getElementById("exampleInputEmail1");
+const password = document.getElementById("exampleInputPassword1");
+const button = document.getElementById("continue");
+
+button.addEventListener("click", function () {
+console.log(email.value)
+});
